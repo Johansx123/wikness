40a75cdbb13eaed97e892f23569f8acc47bab7e7
Bloxtermol10 <alefrito554@gmail.com>
2023-04-17 17:14:49 -0500

    Comieenzo slider

--- ui.js
+++ ui.js
@@ -35,3 +35,63 @@ const pet = () => {
     })
 };
 
+// Slider
+
+// let btnNext = document.getElementById('btn-next')
+// let btnPrev = document.getElementById('btn-prev')
+// let slider =  document.getElementById('slider')
+// let itemSlider = document.querySelectorAll('.item-slider')
+// let position
+
+
+
+
+// btnNext.addEventListener('click', ()=>{
+//   position = slider.style.marginRight
+  
+//   slider.style.marginRight = '370px' + position
+//   slider.style.transition = 'margin 1s'  
+//   console.log( slider.getAttribute("style")  )
+// })
+
+
+  
+  let boxSlider = document.getElementById('wrapper-jobs')
+  const slider = boxSlider.querySelector("#slider");
+  let slide = boxSlider.querySelectorAll(".item-slider");
+  let slideLast = slide[slide.length -1,slide.length - 2];
+  
+  console.log(slideLast)
+  const prevBtn = boxSlider.querySelector ("#btn-prev");
+  const nextBtn = boxSlider.querySelector ("#btn-next");
+  
+  slider.insertAdjacentElement( "afterbegin", slideLast)
+  
+  const moveNext = () =>{
+    let slideFirst = boxSlider.querySelectorAll(".item-slider")[0];
+    slider.style.marginLeft = "-50%"
+    slider.style.transition = "margin 0.5s"
+    setTimeout(()=>{
+      slider.style.transition = "none"
+      slider.insertAdjacentElement("beforeend", slideFirst)
+      slider.style.marginLeft = "-2%"
+      
+      
+  
+    }, 400);
+  }
+  const movePrev = () =>{
+    let slide = boxSlider.querySelectorAll(".item-slider");
+    let slideLast = slide[slide.length - 1, slide.length-2];
+    slider.style.marginLeft = "0%"
+    slider.style.transition = "margin 0.5s"
+    setTimeout(()=>{
+      slider.style.transition = "none"
+      slider.insertAdjacentElement("afterbegin", slideLast)
+      slider.style.marginLeft= "0"
+    }, 500);
+  }
+  
+  nextBtn.addEventListener('click', ()=>{moveNext()})
+  prevBtn.addEventListener('click', ()=>{movePrev()})
+
