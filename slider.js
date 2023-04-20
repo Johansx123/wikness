733d4b76103c3cc07ef6b5ba28c3fc4f3c509e87
Bloxtermol10 <alefrito554@gmail.com>
2023-04-20 18:31:11 -0500

    Responsive slider

--- slider.js
+++ slider.js
@@ -13,7 +13,7 @@ const showHideIcons = () => {
 };
 arrowIcons.forEach((icon) => {
   icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14;
+    let firstImgWidth = firstImg.clientWidth;
     carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
     setTimeout(() => showHideIcons(), 60);
   });
@@ -22,7 +22,7 @@ arrowIcons.forEach((icon) => {
 const autoSlide = () =>{
     if(carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
     positionDiff = Math.abs(positionDiff);
-    let firstImgWidth = firstImg.clientWidth + 14;
+    let firstImgWidth = firstImg.clientWidth;
     let valDifference = firstImgWidth - positionDiff;
 
     if(carousel.scrollLeft > prevScrollLeft){
